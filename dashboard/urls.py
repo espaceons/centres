4ac24828de55b5b685207054946f1456625e5f43
espaceons <espaceons@gmail.com>
2024-12-09 14:44:33 +0100

    ajout administrations du centre

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,6 @@ app_name='dashboard'
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('administrations/', views.administrations, name='administrations'),
+    path('detailadmin/', views.detailadmin, name='detailadmin'),
 ]
-
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request,):
-    return render(request,'dashboard/home.html')
+def home(request):
+    context = {}
+    return render(request,'dashboard/home.html', context)
+
+
+def administrations(request):
+    context = {}
+    return render(request,'centres/administrations.html', context)
+
+
+def detailadmin(request):
+    context= {}
+    return render(request,'centres/detailadministrations.html', context)
